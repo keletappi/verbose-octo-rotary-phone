15b2ac24af8761816b0e0a1f66f91048893fcb9c
Mikko Liikanen <mikko.liikanen@gmail.com>
2018-11-12 00:31:04 +0200

    Code cleanup for tests

--- app/src/test/java/com/mikonoma/elisademo/network/HttpTestBase.kt
+++ app/src/test/java/com/mikonoma/elisademo/network/HttpTestBase.kt
@@ -5,26 +5,10 @@ import org.junit.Before
 import org.mockserver.client.server.MockServerClient
 import org.mockserver.integration.ClientAndServer
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
-
-
 
 internal val random = Random()
 internal fun randomPort(from: Int = 1025, to: Int = 65535) : Int {
     return random.nextInt(to - from) + from
-}
-
-internal val portRange = 1024..65535
-internal val portCounter  = AtomicInteger(1024)
-internal fun port(): Int {
-        synchronized(portCounter) {
-            val nextPort = portCounter.incrementAndGet()
-            if (!(nextPort in portRange)) {
-                portCounter.set(1025)
-                return 1025
-            }
-            return nextPort;
-    }
 }
 
 abstract class HttpTestBase {
